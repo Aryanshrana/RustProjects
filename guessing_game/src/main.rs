295f02c296c6f349176dd3931876628256017477
Aryansh <500093038@stu.upes.ac.in>
2023-07-17 21:33:03 +0530

    modified in guessing_game repo

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..100);
     
     loop {
-        println!("enter your number!");
+        println!("enter your guessing number!");
         let mut guess = String::new();
 
         io::stdin()
