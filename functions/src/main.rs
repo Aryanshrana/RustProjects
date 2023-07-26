846610d0aec4b707d2ddbb2d675ac8da782f8d00
Aryansh <500093038@stu.upes.ac.in>
2023-07-26 22:54:04 +0530

    Adding control flow

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,7 +1,11 @@
+use std::io;
 fn main() {
     println!("Hello, world!");
+    //expression evaluae to a value and statements does not return a value
+    //expressions does not have a ending semicolon if we add we will making it statements
     another_function("Aryansh");
     another_function2(12, 'm');
+
 
     //expression example
     let y = {
@@ -10,7 +14,11 @@ fn main() {
     };
     println!("The value of y is {y}");
     let z = plus_value(100,y);
-    println!("The value of new z is {z}")
+    println!("The value of new z is {z}");
+
+    // we have understood expressions and functions ; here we are introducing control flow with another function
+    control_flow();
+
 }
 fn another_function(x: &str) {
     println!("Hello, again {}",x);
@@ -22,5 +30,21 @@ fn plus_value(z:i32,y:i32) -> i32 {
     println!("The value of z is {z}");
     z+y// -> it is expression so it does not have ending semicolon
 }
-//expression evaluae to a value and statements does not return a value
-//expressions does not have a ending semicolon if we add we will making it statements
+// CONTROL FLOW 
+fn control_flow() {
+    let mut number = String::new();
+    println!("enter a number");
+    io::stdin()
+        .read_line(&mut number)
+        .expect("Failed to read input");
+    let number2: u32 = number.trim().parse().expect("invalid");
+    
+    if number2 % 4 == 0 {
+        println!("Hello Aryansh");
+    }
+    else {
+        println!("Hello Shubham");
+    }
+
+}
+
