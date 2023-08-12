e1d66c14e3f3c850269d69bce45eb34511d01c93
Aryansh <500093038@stu.upes.ac.in>
2023-08-12 16:36:01 +0530

    ownership: return values and scope

--- Ownership/src/main.rs
+++ Ownership/src/main.rs
@@ -1,45 +1,19 @@
 fn main() {
-    //let s = "hello";//string literal stored in stack
-    //another data type String type stored in heap
-    
-    //-->let mut s1 = String::from("hello");//converting string literal to String type
-    
-    //here we are storing it in heap; so String:from is asking for memory at run time
-    
-    //-->s1.push_str(",world!");//push_str append string literal to String
-    //-->println!("{}",s1);
+    // here we are seeing how ownership is changing for a value as it passes to a function or return to a calling function
+    let s1 = gives_ownership(); // gives_ownership moves its return value into s1
+    println!("s1 = {}",s1);
+    let s2 = String::from("welcome"); //s2 comes in scope
+    let s3 = takes_and_gives_back(s2); // s2 moves into takes_and_gives_back which also moves it'sreturn value to s3
+    println!("s3 = {}",s3);
+} // s3 goes out of scope and drop is called , s2 was moved so nothing happens , and s1 goes out of scope so nothing happened
+fn gives_ownership() -> String {
 
-    //to give back the memory when we don't use it
-    //When a variable goes out of scope, Rust calls a special function for us. This function is called drop, and it’s where the author of String can put the code to return the memory. Rust calls drop automatically at the closing curly bracket.
+   let some_string = String::from("you are"); //some_string comes in scope
+   some_string //some_string is returned and moves to function who is calling it
 
-    //Scenarios where owership concept works:
-
-    //vatiables and data interacting with move
-    
-    //-->let mut s2 = s1;
-
-    //here what ponter , length, capacity of s1 is copied to s2 but actual data in heap is same so both s2 and s1 are copied to same data in heap memory which will give us double free memory error if rust try to free the error using drop function twice, which will give us security vulnerabilities, to avoid this what rust do it makes s1 invalid and we say s1 is moved into s2.
-    //we do not deeply copy the data in heap because that will be inexpensive in runtime if data is too big
-    
-    //-->s2.push_str(",backstage!");
-    //-->println!("{}",s2);
-
-    // variables and data interacting with clone 
-    // is we want to do deep copy of the data we can use clone function
-    //-->let mut s3 = s1.clone();
-    //-->println!("s1={},s3={}",s1,s3);
-
-    let s3 = String::from("Bonjour!"); // s3 comes under scope
-    takes_ownership(s3); // we call a function here s3 moves and no longer valid
-
-    let x = 32;//x comes in scope
-    makes_copy(x); //x is copied to another variable and not moved so x is still valid after this
-    println!("{}",x+2);
 }
-//here x goes out of scope and the s3 but is moved so nothing special happens 
-fn takes_ownership(some_string: String){ // some_string is comes into scope
-    println!("some_string = {}",some_string);
-} // some_string goes out of the scope and drop function is called and is no longer valid
-fn makes_copy(some_value:i32){ // some_value comes in scope
-    println!("some_value = {}",some_value+3);
-} // some_value goes out of scope
+fn takes_and_gives_back(mut my_string: String) -> String {
+
+    my_string.push_str(" to our house");
+    my_string //a_string is returned and moves out to calling function
+}
